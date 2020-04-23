ef5477f38998ba9efed740a3ee0fee55dc731964
Rafael Birkmann <mail@rafaelbirkmann.de>
2020-04-24 00:24:57 +0200

    files

--- src/components/intro/intro.js
+++ src/components/intro/intro.js
@@ -4,26 +4,38 @@ import "./intro.scss";
 function Intro() {
   return (
     <div className='intro'>
-      <div className='item fellliebe'>
-        <a href='https://hundesalon-fellliebe.de/' target='_blank'>
+      <div className='item limitbikes'>
+        <a href='http://shop.limit-bikes.de/' target='_blank'>
           <div className='details'>
-            <h3>Hundesalon FellLiebe</h3>
+            <h3>Limit Bikes</h3>
+            <p>Fahrradgeschäft in Burgberg</p>
           </div>
           <div className='image'></div>
         </a>
       </div>
-      <div className='item limitbikes'>
-        <a href='http://shop.limit-bikes.de/' target='_blank'>
+      <div className='item fellliebe'>
+        <a href='https://hundesalon-fellliebe.de/' target='_blank'>
           <div className='details'>
-            <h3>Limit Bikes Burgberg</h3>
+            <h3>FellLiebe</h3>
+            <p>Der mobile Hundefriseur</p>
+          </div>
+          <div className='image'></div>
+        </a>
+      </div>
+      <div className='item test'>
+        <a href='#'>
+          <div className='details'>
+            <h3>Kingpin Skateshop</h3>
+            <p>Online Skateshop</p>
           </div>
           <div className='image'></div>
         </a>
       </div>
       <div className='item car'>
-        <a href=' https://reifen-pabst.de/' target='_blank'>
+        <a href='https://reifen-pabst.de/' target='_blank'>
           <div className='details'>
             <h3>Reifen Allgäu</h3>
+            <p>Reifenservice in Immenstadt</p>
           </div>
           <div className='image'></div>
         </a>
