a6a35a64c4282f53cb77b0908a5a06f7f6a78626
Rafael Birkmann <mail@rafaelbirkmann.de>
2020-04-23 16:06:27 +0200

    files

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -27,6 +27,34 @@ function Header() {
             </div>
           </div>
         </div>
+        <div className='menu'>
+          <div className='container'>
+            <div className='col'>
+              <nav className='main'>
+                <a href=''>Projekte</a>
+                <a href=''>Agentur</a>
+                <a href=''>Leistungen</a>
+                <a href=''>Kontakt</a>
+              </nav>
+              <nav className='sub'>
+                <a href=''>Blog</a>
+              </nav>
+            </div>
+            <div className='col'>
+              <div className='info'>
+                <h4>Sonthofen</h4>
+                <p>Dekan-Ried-Straße 16</p>
+                <p>87527 Sonthofen</p>
+                <p>
+                  <a href='tel:017643471769'>017643471769</a>
+                </p>
+                <p>
+                  <a href='mailto:mail@birkmann.design'>mail@birkmann.design</a>
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
         <main>
           <div className='container'>
             <h2>Design und Kommunikation für eine digitale Welt.</h2>
