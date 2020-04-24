672c1d57e67f5ffd3e54a5c6c6ae1f571b0d2c29
Rafael Birkmann <mail@rafaelbirkmann.de>
2020-04-24 17:51:38 +0200

    files

--- src/App.js
+++ src/App.js
@@ -1,14 +1,6 @@
 import React from "react";
 import Header from "./components/header/header";
 import Intro from "./components/intro/intro";
-
-import ReactDOM from "react-dom";
-import {
-  BrowserRouter as Router,
-  Route,
-  Link,
-  NavLink,
-} from "react-router-dom";
 import "./App.scss";
 
 function App() {
